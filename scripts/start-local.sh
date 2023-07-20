37b6fddeda03fcd824da9dadea48e4092900cbb5
Anton Cheng <antonassocareer@gmail.com>
2023-07-20 16:17:35 +0800

    chore: add output handling

--- scripts/start-local.sh
+++ scripts/start-local.sh
@@ -7,6 +7,9 @@ anvil --host 0.0.0.0 --port 8000 &
 # Allow some time for the anvil server to start up
 sleep 5
 
+# Set the output directory path
+output_dir="/app/output"
+
 # gives this account 10000 ethers to start with <3
 address=$(cast wallet address "$PRIVATE_KEY")
 cast rpc --rpc-url http://localhost:8000 anvil_setBalance "$address" 0x21e19e0c9bab2400000
@@ -15,7 +18,7 @@ cast rpc --rpc-url http://localhost:8000 anvil_setBalance "$address" 0x21e19e0c9
 cd ./lib/v2-core
 forge script scripts/deploy-mocks.s.sol --rpc-url http://localhost:8000/ --broadcast
 forge script scripts/deploy-core.s.sol --rpc-url http://localhost:8000/ --broadcast 
-MARKET_NAME=weth forge script scripts/deploy-market.s.sol --rpc-url http://localhost:8000/ --broadcast && \
+# MARKET_NAME=weth forge script scripts/deploy-market.s.sol --rpc-url http://localhost:8000/ --broadcast && \
 
 # copy the out put of core deployment as input to matching repo's deployment script
 # (from lib/v2-core/deployments/{31337}/core.json to scripts/input/{31337}/config.json)
@@ -35,6 +38,13 @@ cd ../../  && \
 forge build && \
 forge script scripts/deploy-all.s.sol --rpc-url http://localhost:8000/ --broadcast
 
+# Copy deployment data to the output directory
+cp -r /app/lib/v2-core/deployments/31337 "$output_dir"
+cp -r /app/deployments/31337 "$output_dir"
+# Move and rename the config.json file to mocks.json
+mv /app/scripts/input/31337/config.json "$output_dir/mocks.json"
+# Make the output directory accessible from outside the container
+chmod -R 777 "$output_dir"
 
 # Keep the container running
 while true; do sleep 1; done
