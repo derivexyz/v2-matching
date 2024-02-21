04c7c01eee5ccce319ab318717900efa82400ed6
Dillon Lin <30581721+DillonLin@users.noreply.github.com>
2024-02-21 12:00:56 +1100

    Testing (#46)

--- src/periphery/FeeSplitter.sol
+++ src/periphery/FeeSplitter.sol
@@ -1,11 +1,15 @@
 pragma solidity ^0.8.18;
 
 import {Ownable2Step} from "openzeppelin/access/Ownable2Step.sol";
-import "v2-core/src/interfaces/ISubAccounts.sol";
+import {IManager} from "v2-core/src/interfaces/IManager.sol";
+import {ISubAccounts} from "v2-core/src/interfaces/ISubAccounts.sol";
 
 contract FeeSplitter is Ownable2Step {
   ISubAccounts public immutable subAccounts;
   uint public immutable subAcc;
+  uint private splitPercent;
+  uint private accountA;
+  uint private accountB;
 
   constructor(ISubAccounts _subAccounts, IManager manager) {
     subAccounts = _subAccounts;
@@ -16,13 +20,52 @@ contract FeeSplitter is Ownable2Step {
   // Admin //
   ///////////
 
-  function setSplit(uint splitPercent) external onlyOwner {}
+  function setSplit(uint splitPercent) external onlyOwner {
+    require(_splitPercent <= 100, "Invalid percentage");
+    splitPercent = _splitPercent;
+  }
 
-  function setSubAccounts(uint accountA, uint accountB) external onlyOwner {}
+  function setSubAccounts(uint _accountA, uint _accountB) external onlyOwner {
+    accountA = _accountA;
+    accountB = _accountB;
+  }
 
   //////////////
   // External //
   //////////////
   /// @notice Work out the balance of the subaccount held by this contract, and split it based on the % split
-  function split() external {}
+  function split() external {
+    ISubAccounts.AssetBalance[] memory balances = subAccounts.getAccountBalances(subAcc);
+
+    for (uint i = 0; i < balances.length; i++) {
+        IAsset asset = balances[i].asset;
+        uint subId = balances[i].subId;
+        int balance = balances[i].balance;
+
+        if (balance > 0) {
+            uint splitAmountA = uint(balance) * splitPercent / 100;
+            uint splitAmountB = uint(balance) - splitAmountA;
+
+            ISubAccounts.AssetTransfer[] memory transfers = new ISubAccounts.AssetTransfer[](2);
+            transfers[0] = ISubAccounts.AssetTransfer({
+                fromAcc: subAcc,
+                toAcc: accountA,
+                asset: asset,
+                subId: subId,
+                amount: int(splitAmountA),
+                assetData: bytes32(0)
+            });
+            transfers[1] = ISubAccounts.AssetTransfer({
+                fromAcc: subAcc,
+                toAcc: accountB,
+                asset: asset,
+                subId: subId,
+                amount: int(splitAmountB),
+                assetData: bytes32(0)
+            });
+
+            subAccounts.submitTransfers(transfers, "");
+        }
+    }
+  }
 }
--- test/periphery/FeeSplitter.t.sol
+++ test/periphery/FeeSplitter.t.sol
@@ -1,19 +1,40 @@
 pragma solidity ^0.8.18;
 
-import "../../src/periphery/FeeSplitter.sol";
-import "v2-core/test/integration-tests/shared/IntegrationTestBase.t.sol";
+import {IntegrationTestBase} from "v2-core/test/integration-tests/shared/IntegrationTestBase.t.sol";
+import {IManager} from "v2-core/src/interfaces/IManager.sol";
+import {IAsset} from "v2-core/src/interfaces/IAsset.sol";
+import {ISubAccounts} from "v2-core/src/interfaces/ISubAccounts.sol";
 
+import "forge-std/console2.sol";
+import "../../src/periphery/FeeSplitter.sol";
 
 contract FeeSplitterTest is IntegrationTestBase {
   FeeSplitter public feeSplitter;
+  IAsset public asset;
+  uint public accountA;
+  uint public accountB;
 
-  function setUp() public {
-    _setupIntegrationTestComplete();
-
+  function setUp() public override {
+    super.setUp();
     feeSplitter = new FeeSplitter(subAccounts, srm);
+    feeSplitter.setSubAccounts(accountA, accountB);
+    feeSplitter.setSplit(50);
+    uint amount = 100e18;
+    cashAsset.mint(address(this), amount);
+    cashAsset.approve(address(subAccounts), amount);
+    cashAsset.deposit(feeSplitter.subAcc(), amount);
   }
 
   function test_split() public {
+    // before split
+    assertEq(subAccounts.getBalance(feeSplitter.subAcc(), cashAsset, 0), int(100e18), "Incorrect initial balance in FeeSplitter");
+    assertEq(subAccounts.getBalance(accountA, cashAsset, 0), 0, "Account A should initially have 0 balance");
+    assertEq(subAccounts.getBalance(accountB, cashAsset, 0), 0, "Account B should initially have 0 balance");
+
     feeSplitter.split();
+
+    // after split
+    assertEq(subAccounts.getBalance(accountA, cashAsset, 0), int(50e18), "Account A should have half of the funds after split");
+    assertEq(subAccounts.getBalance(accountB, cashAsset, 0), int(50e18), "Account B should have half of the funds after split");
   }
 }
