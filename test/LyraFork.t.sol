5321314a1aac5c5c35c87c7e23ffe794663a94e8
DomRom <romanowski.dominic@gmail.com>
2024-08-27 08:40:34 +1000

    Feat/stable deploys (#55)
    * updated deploy script
    
    * updated stable deploys
    
    * skip fork

--- test/LyraFork.t.sol
+++ test/LyraFork.t.sol
@@ -19,35 +19,130 @@ contract LyraForkTest is ForkBase {
 
     StandardManager srm = StandardManager(_getContract("core", "srm"));
 
-    uint marketId = srm.createMarket("sUSDe");
-    console.log("marketId:", marketId);
+    {
+      uint marketId = srm.createMarket("DAI");
+      console.log("marketId:", marketId);
 
-    srm.whitelistAsset(IAsset(_getContract("sUSDe", "base")), marketId, IStandardManager.AssetType.Base);
-    srm.setOraclesForMarket(
-      marketId, ISpotFeed(_getContract("sUSDe", "spotFeed")), IForwardFeed(address(0)), IVolFeed(address(0))
-    );
+      srm.setOracleContingencyParams(
+        marketId,
+        IStandardManager.OracleContingencyParams({
+          perpThreshold: 0.55e18,
+          optionThreshold: 0.55e18,
+          baseThreshold: 0.55e18,
+          OCFactor: 1e18
+        })
+      );
+      srm.setBaseAssetMarginFactor(marketId, 0.8e18, 0.6e18);
+      srm.setOraclesForMarket(
+        marketId, ISpotFeed(_getContract("DAI", "spotFeed")), IForwardFeed(address(0)), IVolFeed(address(0))
+      );
 
-    srm.setOracleContingencyParams(
-      marketId,
-      IStandardManager.OracleContingencyParams({
-        perpThreshold: 0.55e18,
-        optionThreshold: 0.55e18,
-        baseThreshold: 0.55e18,
-        OCFactor: 1e18
-      })
-    );
+      srm.whitelistAsset(IAsset(_getContract("DAI", "base")), marketId, IStandardManager.AssetType.Base);
 
-    srm.setBaseAssetMarginFactor(marketId, 0.8e18, 0.6e18);
+      SRMPortfolioViewer srmViewer = SRMPortfolioViewer(_getContract("core", "srmViewer"));
+      srmViewer.setOIFeeRateBPS(_getContract("DAI", "base"), 0.5e18);
 
-    SRMPortfolioViewer srmViewer = SRMPortfolioViewer(_getContract("core", "srmViewer"));
-    srmViewer.setOIFeeRateBPS(_getContract("sUSDe", "base"), 0.5e18);
+      srm.setWhitelistedCallee(_getContract("DAI", "spotFeed"), true);
+      PMRM(_getContract("ETH", "pmrm")).setWhitelistedCallee(_getContract("DAI", "spotFeed"), true);
+      PMRM(_getContract("BTC", "pmrm")).setWhitelistedCallee(_getContract("DAI", "spotFeed"), true);
 
-    srm.setWhitelistedCallee(_getContract("sUSDe", "spotFeed"), true);
-    PMRM(_getContract("ETH", "pmrm")).setWhitelistedCallee(_getContract("sUSDe", "spotFeed"), true);
-    PMRM(_getContract("BTC", "pmrm")).setWhitelistedCallee(_getContract("sUSDe", "spotFeed"), true);
+      Ownable2Step(_getContract("DAI", "base")).acceptOwnership();
+      Ownable2Step(_getContract("DAI", "spotFeed")).acceptOwnership();
+    }
+    {
+      uint marketId = srm.createMarket("sDAI");
+      console.log("marketId:", marketId);
 
-    Ownable2Step(_getContract("sUSDe", "base")).acceptOwnership();
-    Ownable2Step(_getContract("sUSDe", "spotFeed")).acceptOwnership();
+      srm.whitelistAsset(IAsset(_getContract("sDAI", "base")), marketId, IStandardManager.AssetType.Base);
+      srm.setOraclesForMarket(
+        marketId, ISpotFeed(_getContract("sDAI", "spotFeed")), IForwardFeed(address(0)), IVolFeed(address(0))
+      );
+
+      srm.setOracleContingencyParams(
+        marketId,
+        IStandardManager.OracleContingencyParams({
+          perpThreshold: 0.55e18,
+          optionThreshold: 0.55e18,
+          baseThreshold: 0.55e18,
+          OCFactor: 1e18
+        })
+      );
+
+      srm.setBaseAssetMarginFactor(marketId, 0.8e18, 0.6e18);
+
+      SRMPortfolioViewer srmViewer = SRMPortfolioViewer(_getContract("core", "srmViewer"));
+      srmViewer.setOIFeeRateBPS(_getContract("sDAI", "base"), 0.5e18);
+
+      srm.setWhitelistedCallee(_getContract("sDAI", "spotFeed"), true);
+      PMRM(_getContract("ETH", "pmrm")).setWhitelistedCallee(_getContract("sDAI", "spotFeed"), true);
+      PMRM(_getContract("BTC", "pmrm")).setWhitelistedCallee(_getContract("sDAI", "spotFeed"), true);
+
+      Ownable2Step(_getContract("sDAI", "base")).acceptOwnership();
+      Ownable2Step(_getContract("sDAI", "spotFeed")).acceptOwnership();
+    }
+    {
+      uint marketId = srm.createMarket("USDe");
+      console.log("marketId:", marketId);
+
+      srm.whitelistAsset(IAsset(_getContract("USDe", "base")), marketId, IStandardManager.AssetType.Base);
+      srm.setOraclesForMarket(
+        marketId, ISpotFeed(_getContract("USDe", "spotFeed")), IForwardFeed(address(0)), IVolFeed(address(0))
+      );
+
+      srm.setOracleContingencyParams(
+        marketId,
+        IStandardManager.OracleContingencyParams({
+          perpThreshold: 0.55e18,
+          optionThreshold: 0.55e18,
+          baseThreshold: 0.55e18,
+          OCFactor: 1e18
+        })
+      );
+
+      srm.setBaseAssetMarginFactor(marketId, 0.8e18, 0.6e18);
+
+      SRMPortfolioViewer srmViewer = SRMPortfolioViewer(_getContract("core", "srmViewer"));
+      srmViewer.setOIFeeRateBPS(_getContract("USDe", "base"), 0.5e18);
+
+      srm.setWhitelistedCallee(_getContract("USDe", "spotFeed"), true);
+      PMRM(_getContract("ETH", "pmrm")).setWhitelistedCallee(_getContract("USDe", "spotFeed"), true);
+      PMRM(_getContract("BTC", "pmrm")).setWhitelistedCallee(_getContract("USDe", "spotFeed"), true);
+
+      Ownable2Step(_getContract("USDe", "base")).acceptOwnership();
+      Ownable2Step(_getContract("USDe", "spotFeed")).acceptOwnership();
+    }
+    {
+      uint marketId = srm.createMarket("PYUSD");
+      console.log("marketId:", marketId);
+
+      srm.whitelistAsset(IAsset(_getContract("PYUSD", "base")), marketId, IStandardManager.AssetType.Base);
+      srm.setOraclesForMarket(
+        marketId, ISpotFeed(_getContract("PYUSD", "spotFeed")), IForwardFeed(address(0)), IVolFeed(address(0))
+      );
+
+      srm.setOracleContingencyParams(
+        marketId,
+        IStandardManager.OracleContingencyParams({
+          perpThreshold: 0.55e18,
+          optionThreshold: 0.55e18,
+          baseThreshold: 0.55e18,
+          OCFactor: 1e18
+        })
+      );
+
+      srm.setBaseAssetMarginFactor(marketId, 0.8e18, 0.6e18);
+
+      SRMPortfolioViewer srmViewer = SRMPortfolioViewer(_getContract("core", "srmViewer"));
+      srmViewer.setOIFeeRateBPS(_getContract("PYUSD", "base"), 0.5e18);
+
+      srm.setWhitelistedCallee(_getContract("PYUSD", "spotFeed"), true);
+      PMRM(_getContract("ETH", "pmrm")).setWhitelistedCallee(_getContract("PYUSD", "spotFeed"), true);
+      PMRM(_getContract("BTC", "pmrm")).setWhitelistedCallee(_getContract("PYUSD", "spotFeed"), true);
+
+      Ownable2Step(_getContract("PYUSD", "base")).acceptOwnership();
+      Ownable2Step(_getContract("PYUSD", "spotFeed")).acceptOwnership();
+      WrappedERC20Asset(_getContract("PYUSD", "base")).setTotalPositionCap(srm, 0);
+    }
   }
 
   function _getContract(string memory file, string memory name) internal view returns (address) {
