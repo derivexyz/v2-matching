24ae0c001b4a565eb3259283f1a0f645ed1bd675
Dominic Romanowski <romanowski.dominic@gmail.com>
2024-10-30 16:36:21 +1100

    add calldata printing in fork test

--- test/fork/PerpMarketFork.sol
+++ test/fork/PerpMarketFork.sol
@@ -12,8 +12,15 @@ import "./ForkBase.t.sol";
 import "v2-core/scripts/config-mainnet.sol";
 
 contract PerpMarketFork is ForkBase {
+  struct CD {
+    address addr;
+    bytes data;
+  }
+
   string[10] public MARKETS = ["LINK", "XRP", "AVAX", "UNI", "TAO", "SEI", "EIGEN", "ENA", "BITCOIN", "DEGEN"];
   string[4] public feeds = ["spotFeed", "perpFeed", "iapFeed", "ibpFeed"];
+
+  CD[] public calldatas;
 
   /**
    * [COIN, IM leverage, MM leverage, OI cap]
@@ -25,12 +32,12 @@ contract PerpMarketFork is ForkBase {
    * SEI,    5x,         15x,         1M
    * EIGEN,  5x,         15x,         150K
    * ENA     10k,        15k,         1M
-   * BITCOIN,3k,         5k,          600k
-   * DEGEN,  3k,         5k,          20M
+   * BITCOIN,3x,         5x,          600k
+   * DEGEN,  3x,         5x,          20M
    */
   function setUp() external {}
 
-  function testFork() external skipped {
+  function testFork() external {
     vm.deal(address(0xB176A44D819372A38cee878fB0603AEd4d26C5a5), 1 ether);
     vm.startPrank(0xB176A44D819372A38cee878fB0603AEd4d26C5a5);
 
@@ -39,45 +46,59 @@ contract PerpMarketFork is ForkBase {
     PMRM ethPMRM = PMRM(0xe7cD9370CdE6C9b5eAbCe8f86d01822d3de205A0);
     PMRM btcPMRM = PMRM(0x45DA02B9cCF384d7DbDD7b2b13e705BADB43Db0D);
 
+    // store every function calldata as a pair of (address, calldata) in a string array
+
     for (uint i = 0; i < MARKETS.length; i++) {
       string memory market = MARKETS[i];
       console.log("market:", market);
       string memory deploymentFile = _readV2CoreDeploymentFile(market);
 
       uint marketId = srm.createMarket(market);
-      console.log("marketId:", marketId);
+      calldatas.push(CD(address(srm), abi.encodeWithSelector(srm.createMarket.selector, market)));
 
+      console.log("marketId:", marketId);
       (
         IStandardManager.PerpMarginRequirements memory perpMarginRequirements,
         ,
         IStandardManager.OracleContingencyParams memory oracleContingencyParams,
       ) = Config.getSRMParams(market);
 
-      srm.setOracleContingencyParams(marketId, oracleContingencyParams);
-
-      srm.setPerpMarginRequirements(marketId, perpMarginRequirements.mmPerpReq, perpMarginRequirements.imPerpReq);
-      srm.setOraclesForMarket(
-        marketId, ISpotFeed(_getContract(deploymentFile, "spotFeed")), IForwardFeed(address(0)), IVolFeed(address(0))
-      );
-
-      srm.whitelistAsset(IAsset(_getContract(deploymentFile, "perp")), marketId, IStandardManager.AssetType.Perpetual);
+      calldatas.push(_call(address(srm), abi.encodeWithSelector(srm.setOracleContingencyParams.selector, marketId, oracleContingencyParams)));
+      calldatas.push(_call(address(srm), abi.encodeWithSelector(srm.setPerpMarginRequirements.selector, marketId, perpMarginRequirements.mmPerpReq, perpMarginRequirements.imPerpReq)));
+      calldatas.push(_call(address(srm), abi.encodeWithSelector(srm.setOraclesForMarket.selector, marketId, ISpotFeed(_getContract(deploymentFile, "spotFeed")), IForwardFeed(address(0)), IVolFeed(address(0)))));
+      calldatas.push(_call(address(srm), abi.encodeWithSelector(srm.whitelistAsset.selector, IAsset(_getContract(deploymentFile, "perp")), marketId, IStandardManager.AssetType.Perpetual)));
 
       SRMPortfolioViewer srmViewer = SRMPortfolioViewer(_getV2CoreContract("core", "srmViewer"));
-      srmViewer.setOIFeeRateBPS(_getContract(deploymentFile, "perp"), 0.1e18);
+      calldatas.push(_call(address(srmViewer), abi.encodeWithSelector(srmViewer.setOIFeeRateBPS.selector, _getContract(deploymentFile, "perp"), 0.5e18)));
 
       for (uint j = 0; j < feeds.length; j++) {
         string memory feed = feeds[j];
         console.log("feed:", feed);
-        srm.setWhitelistedCallee(_getContract(deploymentFile, feed), true);
-        ethPMRM.setWhitelistedCallee(_getContract(deploymentFile, feed), true);
-        btcPMRM.setWhitelistedCallee(_getContract(deploymentFile, feed), true);
+        calldatas.push(_call(address(srm), abi.encodeWithSelector(srm.setWhitelistedCallee.selector, _getContract(deploymentFile, feed), true)));
+        calldatas.push(_call(address(ethPMRM), abi.encodeWithSelector(ethPMRM.setWhitelistedCallee.selector, _getContract(deploymentFile, feed), true)));
+        calldatas.push(_call(address(btcPMRM), abi.encodeWithSelector(btcPMRM.setWhitelistedCallee.selector, _getContract(deploymentFile, feed), true)));
       }
 
-      Ownable2Step(_getContract(deploymentFile, "perp")).acceptOwnership();
-      Ownable2Step(_getContract(deploymentFile, "spotFeed")).acceptOwnership();
-      Ownable2Step(_getContract(deploymentFile, "perpFeed")).acceptOwnership();
-      Ownable2Step(_getContract(deploymentFile, "iapFeed")).acceptOwnership();
-      Ownable2Step(_getContract(deploymentFile, "ibpFeed")).acceptOwnership();
+      calldatas.push(_call(_getContract(deploymentFile, "perp"), abi.encodeWithSelector(Ownable2Step.acceptOwnership.selector)));
+      calldatas.push(_call(_getContract(deploymentFile, "spotFeed"), abi.encodeWithSelector(Ownable2Step.acceptOwnership.selector)));
+      calldatas.push(_call(_getContract(deploymentFile, "perpFeed"), abi.encodeWithSelector(Ownable2Step.acceptOwnership.selector)));
+      calldatas.push(_call(_getContract(deploymentFile, "iapFeed"), abi.encodeWithSelector(Ownable2Step.acceptOwnership.selector)));
+      calldatas.push(_call(_getContract(deploymentFile, "ibpFeed"), abi.encodeWithSelector(Ownable2Step.acceptOwnership.selector)));
     }
+
+    //print all the function calldata
+    uint txsPerMarket = calldatas.length / MARKETS.length;
+    for (uint i = 0; i < txsPerMarket; i++) {
+      for (uint j = 0; j < MARKETS.length; j++) {
+        console.log(calldatas[j * txsPerMarket + i].addr, ",");
+        console.logBytes(calldatas[j * txsPerMarket + i].data);
+      }
+    }
+  }
+
+  function _call(address addr, bytes memory data) internal returns (CD memory) {
+    (bool success, bytes memory ret) = addr.call(data);
+    require(success, string(ret));
+    return CD(addr, data);
   }
 }
